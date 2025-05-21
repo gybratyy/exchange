b6e0f8f82b2dd494979238648d4abf6d4171c7b6
gybratyy <220001@astanait.edu.kz>
2025-05-22 00:00:53 +0500

    deployment debug

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,14 +31,15 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/books", bookRoutes)
-/*app.use("/api/user", userRoutes)*/
+app.use("/api/user", userRoutes)
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+    app.get(/^\/(?!api).*/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+
 }
 
 server.listen(PORT, () => {
